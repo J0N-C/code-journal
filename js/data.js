edcfd0f9b513b7738b191b3ddba35e7c1200e12f
Jonathan Chiang <jc91765@gmail.com>
2021-08-19 20:19:23 -0700

    listen for submit events up to reset form

--- js/data.js
+++ js/data.js
@@ -6,3 +6,19 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+const $form = document.querySelector('#code-form');
+
+function submitForm(event) {
+  event.preventDefault();
+  const filledForm = {};
+  filledForm.entryID = data.nextEntryId;
+  filledForm.title = $form.title.value;
+  filledForm.photourl = $form.photo.value;
+  filledForm.notes = $form.notes.value;
+  data.nextEntryId++;
+  $form.reset();
+  data.entries.push(filledForm);
+}
+
+$form.addEventListener('submit', submitForm);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 /* global data */
 /* exported data */
-const $photoUrl = document.querySelector('#entry-photo');
+const $photoUrl = document.querySelector('#photolink');
+const $photo = document.querySelector('#photo');
 
 function handleInput(event) {
-  $photoUrl.setAttribute('src', $photoUrl.value);
+  $photo.setAttribute('src', $photoUrl.value);
 }
 
 $photoUrl.addEventListener('input', handleInput);
+document.addEventListener('submit', function (event) {
+  event.preventDefault();
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+});

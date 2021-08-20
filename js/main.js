1a8fe95a9b023496621f1de8d9f5934f3b761032
Jonathan Chiang <jc91765@gmail.com>
2021-08-19 19:06:33 -0700

    listen for input on photoURL

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+const $photoUrl = document.querySelector('#entry-photo');
+
+function handleInput(event) {
+  $photoUrl.setAttribute('src', $photoUrl.value);
+}
+
+$photoUrl.addEventListener('input', handleInput);
